a905ce9041f1a59d117eb180710cac22c93d6bee
Jakub <Swayer@vp.pl>
2022-05-25 12:19:11 +0200

    Fix weather raport

--- app.js
+++ app.js
@@ -13,30 +13,47 @@ let description = [];
 let z = 0;
 
 app.get('/', (req, res) => {
+  z = 0;
   for (var i = 0; i < city.length; i++) {
-    console.log(i + "kek")
+
     url = "https://api.openweathermap.org/data/2.5/weather?q=" + city[i] + "&appid=f1231941a3a7d645b987724ca5d85de0&units=metric";
-        console.log(i + "mek")
+
     https.get(url, (response) => {
-              
-          console.log(z + "tek")
       response.on("data", (data) => {
-            console.log(z + "hek")
-        weather[i] = JSON.parse(data);
-      temp[i] = Math.round(weather[i].main.temp);
-        // console.log(i);
-        // console.log(temp[i]);
-        description[i] = weather[i].weather[0].description;
-        z++;
+
+        let Data = JSON.parse(data);
+        switch (Data.name) {
+          case "New York":
+            z = 0;
+            temp[z] = Math.round(Data.main.temp);
+            description[z] = Data.weather[0].description;
+            break;
+          case "Helsinki":
+            z = 1;
+            temp[z] = Math.round(Data.main.temp);
+            description[z] = Data.weather[0].description;
+            break;
+          case "Paris":
+            z = 2;
+            temp[z] = Math.round(Data.main.temp);
+            description[z] = Data.weather[0].description;
+            break;
+          case "Tokyo":
+            z = 3;
+            temp[z] = Math.round(Data.main.temp);
+            description[z] = Data.weather[0].description;
+            break;
+        }
       })
     })
   }
 
 
-setTimeout(() => {
-  console.log(temp[0]);
-  res.render('index', {
-        TempNY: temp,
+
+  setTimeout(() => {
+      console.log(temp);
+      res.render('index', {
+        TempNY: temp[0],
         TempPAR: temp[1],
         TempHEL: temp[2],
         TempTOK: temp[3],
@@ -44,8 +61,9 @@ setTimeout(() => {
         DescriptionPAR: description[1],
         DescriptionHEL: description[2],
         DescriptionTOK: description[3],
-      })},
-      2000);
+      })
+    },
+    2000);
 
 })
 
@@ -74,24 +92,24 @@ app.listen(process.env.PORT || 3000, () => {
 //       DescriptionTOK: description[3],
 //     }
 
-    // https.get(url, (resp) => {
-    //   response.on("data", (data) => {
-    //     weather[i] = JSON.parse(data);
-    //     var temp[i] = Math.round(weather[i].main.temp);
-    //     var description[i] = weather[i].weather[0].description;
-    //     console.log(temp[i] + " " + description[i])
-    //   })
-    // })
+// https.get(url, (resp) => {
+//   response.on("data", (data) => {
+//     weather[i] = JSON.parse(data);
+//     var temp[i] = Math.round(weather[i].main.temp);
+//     var description[i] = weather[i].weather[0].description;
+//     console.log(temp[i] + " " + description[i])
+//   })
+// })
 
 
-    // response.on("data", (data) => {
-    //   var data = JSON.parse(data);
-    //
-    // })
-    //
-    // weather[i].temp = Math.round(weather[i].main.temp);
-    // weather[i].description = weather[i].weather[0].description;
-    // res.render('index', {
-    //  ETemp: temp,
-    //  EDescription: description
-    // });
+// response.on("data", (data) => {
+//   var data = JSON.parse(data);
+//
+// })
+//
+// weather[i].temp = Math.round(weather[i].main.temp);
+// weather[i].description = weather[i].weather[0].description;
+// res.render('index', {
+//  ETemp: temp,
+//  EDescription: description
+// });
