1e9bce1e4b1560d301eb4fde15ce9bb3793e6c4e
Jakub <Swayer@vp.pl>
2022-05-25 13:01:33 +0200

    Script update

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+var city = ["NY","PAR","HEL","TOK"]
+var timeZone = [-6,0,1,7]
+
 function Clock() {
 
   var d = new Date();
@@ -6,11 +9,16 @@ function Clock() {
   var minute = (d.getMinutes() * 6) + (d.getSeconds() * 0.1);
   var second = d.getSeconds() * 6;
 
-  var h = "rotate(" + hour + "deg)";
+  // var h = "rotate(" + hour + "deg)";
+  var h = []
   var m = "rotate(" + minute + "deg)";
   var s = "rotate(" + second + "deg)";
 
-  $('.hour').css('transform', h);
+for (var i = 0; i < city.length; i++) {
+  h[i] = "rotate(" + (hour + timeZone[i]*30)+ "deg)";
+  $('#' + city[i]).css('transform', h[i]);
+}
+  // $('.hour').css('transform', h);
   $('.minute').css('transform', m);
   $('.second').css('transform', s);
 
